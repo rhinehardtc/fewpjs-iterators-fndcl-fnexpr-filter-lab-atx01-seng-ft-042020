5039207d01bbad40fef620ac26377229487522f1
rhinehardtc <rhinehardtc@gmail.com>
2020-06-25 23:14:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+function findMatching(drivers, string){
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().indexOf(string.toLowerCase()) !== -1
+    });
+};
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().indexOf(string.toLowerCase()) === 0
+    });
+};
+
+function matchName(drivers, string){
+    return drivers.filter( driver => driver.name === string)
+}
